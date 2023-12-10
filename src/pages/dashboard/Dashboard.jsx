cc9716a8e4e3a66639ea82698f38e0d907f99fba
Amadou Boubacar Niang <71797475+niangamadou888@users.noreply.github.com>
2023-12-10 10:30:53 +0000

    Update Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router';
 import Navbar from './components/Navbar';
 import { Box, Stack, Avatar, Typography } from '@mui/material';
 import AjouterPublication from './components/AjouterPublication';
@@ -7,37 +7,40 @@ import axios from 'axios';
 import { useQueryClient, useQuery } from '@tanstack/react-query';
 import CartePub from './components/CartePub';
 
-
 export default function Dashboard() {
-    const navigate = useNavigate();
-    useEffect(()=>{
-        if(!localStorage.getItem("utilisateur")){
-            navigate("/connexion")
-        }
-    })
-    const queryClient = useQueryClient();
-    const {data:publications, error, isLoading} = useQuery({
-        queryKey: ["publications"],
-        queryFn: () => axios.get("https://mini-social-network.onrender.com/publications").then((res) => res.data),
-        onerror: (error) => console.log(error),
-    })
-    if (isLoading){
-        return <div>chargement</div>
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("utilisateur")) {
+      navigate("/connexion");
     }
+  }, []);
 
-    let pubTrier = publications.sort((a,b) =>{
-        return new Date(b.datePublication) - new Date(a.datePublication);
-    })
+  const queryClient = useQueryClient();
 
-    return (
-        <Box bgcolor={"#ffeeff"}>
-        <Navbar />
-        <AjouterPublication />
-        <Box width={"60%"} margin={"auto"} marginTop={4}>
-        {publications && pubTrier.map((publication) => (
-    <CartePub key={publication.id} publication={publication} />
-))}
-        </Box>
+  const { data: publications, error, isLoading } = useQuery({
+    queryKey: ["publications"],
+    queryFn: () => axios.get("https://mini-social-network.onrender.com/publications").then((res) => res.data),
+    onError: (error) => console.log(error),
+  });
+
+  if (isLoading) {
+    return <div>Chargement</div>;
+  }
+
+  const pubTrier = [...publications].sort((a, b) => {
+    return new Date(b.datePublication) - new Date(a.datePublication);
+  });
+
+  return (
+    <Box bgcolor="#ffeeff">
+      <Navbar />
+      <AjouterPublication />
+      <Box width="60%" margin="auto" marginTop={4}>
+        {pubTrier.map((publication) => (
+          <CartePub key={publication.id} publication={publication} />
+        ))}
+      </Box>
     </Box>
-    );
+  );
 }
